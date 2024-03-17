dde3d19f258695aadbcc5359d4a8c0fd79545ab9
bienbeo <tranngocbien2809@gmail.com>
2024-03-17 14:36:57 +0700

    New src of the 71A exercise

---
+++ 71A_Way_Too_Long_Words.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void run ();
+void exportArr (char arr[]);
+
+int main ()
+{
+    int N; cin >> N;
+    while (N--)
+        run ();
+}
+
+void run ()
+{
+    char arr[100];
+    int cnt = 0;
+    for (int i = 0; i < 101; i++)
+    {
+        cin >> arr[i];
+        cnt += 1;
+    }
+    if (cnt > 9) 
+    {
+        exportArr (arr);
+        cout << endl;
+    }
+    else 
+    {
+        cout << arr[0] << cnt - 1 << arr[cnt];
+    }
+}
+
+void exportArr (char arr[])
+{
+    for (int i = 0; i < 101; i++)
+        cout << arr[i];
+}
